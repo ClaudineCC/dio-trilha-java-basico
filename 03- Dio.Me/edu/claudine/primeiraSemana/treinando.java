2caab96d7ef65d7a21dd98da539816dd2046cb9b
ClaudineCC <159174732+ClaudineCC@users.noreply.github.com>
2024-07-14 22:59:43 -0300

    Add files via upload

---
+++ 03- Dio.Me/edu/claudine/primeiraSemana/treinando.java
@@ -0,0 +1,37 @@
+package edu.claudine.primeiraSemana;
+
+
+public class treinando{    // minha classe
+
+    public static void main(String[] args) {  //metodo principal
+
+        System.out.println("ola ");  // classe de saida out
+        System.out.println("nova linha de impressao");
+
+        // String BR = "Brasil";  // atributos com valores
+        // int ano = 2024;
+        // boolean estudando = true;
+
+        // int numero1 = 10;
+        // int numero2 = 20;
+        // int somar = somar(int numero1,int numero2);
+
+
+        String colecao = ["Claudine", "Iris", "Igor" , "Valter"];
+        System.out.println(String colecao);
+
+
+        // System.out.println("ola, moro no  " +BR+ " estamos no ano " +ano+ " e atualmente estou " +estudando);
+        
+        // public static int somar(int numero1,int numero2){
+        //     return "resultado das somas :" +numero1.concat(" ").concat(numero2);
+
+
+        //     //System.out.println("a soma dos numeros" +numero+ "e" +numero2+ "é igual à :" +somar);    
+
+
+
+        }
+        
+    }
+
